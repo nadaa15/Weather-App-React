347fa12d0ec89fa4339964c4282d7e35348f68dc
Your Name <na835126@gmail.com>
2025-01-05 02:11:00 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Weather-App-React",
+  base: "/Weather-App-React",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
-})
+});
