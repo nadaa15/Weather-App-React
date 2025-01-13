f2fac269a86451606d6e299cd7b04682c1605784
Your Name <na835126@gmail.com>
2025-01-13 15:22:49 +0200

    solved an issue in search functionality

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
 
   function handleSearch(term) {
    setcityError(false)
-   setsearchValue(term.trim());
+   setsearchValue(term);
    dispatch(getWeatherBySearch(term));
  }
 
